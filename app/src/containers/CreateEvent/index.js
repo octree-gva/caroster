e169aeb694c522258a9cbdca0e2ada0eb8d4a417
Karian Før <karian.for@gmail.com>
2020-07-02 11:48:17 +0200

    fix form on safari

--- app/src/containers/CreateEvent/index.js
+++ app/src/containers/CreateEvent/index.js
@@ -1,23 +1,23 @@
-import React, { useState, useReducer } from "react";
-import { useStrapi } from "strapi-react-context";
+import React, {useState, useReducer} from 'react';
+import {useStrapi} from 'strapi-react-context';
 
 // Steps
-import Step1 from "./Step1";
-import Step2 from "./Step2";
+import Step1 from './Step1';
+import Step2 from './Step2';
 
 const steps = [Step1, Step2];
 
-const eventReducer = (state, item) => ({ ...state, ...item });
+const eventReducer = (state, item) => ({...state, ...item});
 
-const CreateEvent = (props) => {
+const CreateEvent = props => {
   const strapi = useStrapi();
   const [step, setStep] = useState(0);
   const [event, addToEvent] = useReducer(eventReducer, {});
   const Step = steps[step];
 
-  const createEvent = async (eventData) => {
+  const createEvent = async eventData => {
     try {
-      return await strapi.services.events.create({ ...event, ...eventData });
+      return await strapi.services.events.create({...event, ...eventData});
     } catch (err) {
       console.error(err);
       return false;
