609aad2801eb89ce7d836319485b3c03432cfaec
Tim Izzo <tim@octree.ch>
2020-07-03 17:23:57 +0200

    ✨ Go to landing page when logo is clicked

--- app/src/components/Logo/index.js
+++ app/src/components/Logo/index.js
@@ -3,9 +3,15 @@ import {makeStyles} from '@material-ui/core/styles';
 
 import LogoUrl from '../../assets/logo.png';
 
+const LOGO_URL = 'https://caroster.io';
+
 const Logo = () => {
   const classes = useStyles();
-  return <img src={LogoUrl} alt="Caroster" className={classes.logo} />;
+  return (
+    <a href={LOGO_URL}>
+      <img src={LogoUrl} alt="Caroster" className={classes.logo} />
+    </a>
+  );
 };
 
 const useStyles = makeStyles(theme => ({
