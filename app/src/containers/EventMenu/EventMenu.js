550370ebaa750b5433b92c35d407de6f7f363911
Hadrien Froger <hadrien@octree.ch>
2020-07-16 13:06:43 +0100

    start EventMenu

---
+++ app/src/containers/EventMenu/EventMenu.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const EventMenu = ({anchorEl, setAnchorEl, actions = []}) => {
+  return (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={!!anchorEl}
+      onClose={() => setAnchorEl(null)}
+    >
+      {actions &&
+        actions.map((action, idx) => (
+          <MenuItem
+            onClick={() => {
+              action.onClick();
+              setAnchorEl(null);
+            }}
+            key={idx}
+            id={action.id || `MenuItem${idx}`}
+          >
+            {action.label}
+          </MenuItem>
+        ))}
+    </Menu>
+  );
+};
+export default EventMenu;
--- app/src/setupTests.js
+++ app/src/setupTests.js
@@ -4,3 +4,20 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 import './i18n';
+
+// FROM https://github.com/akiran/react-slick/blob/master/test-setup.js
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
+
+window.requestAnimationFrame =
+  window.requestAnimationFrame ||
+  function (callback) {
+    setTimeout(callback, 0);
+  };
