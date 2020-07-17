2f4d07042f9675732a9a3de4c6d3ec22bbcec360
Karian Før <karian.for@gmail.com>
2020-07-17 16:46:06 +0200

    corrections

--- app/src/hooks/useProfile.js
+++ app/src/hooks/useProfile.js
@@ -1,26 +1,25 @@
 import {useEffect, useState} from 'react';
-import {useStrapi, useAuth} from 'strapi-react-context';
+import {useAuth} from 'strapi-react-context';
 
 export default () => {
-  const strapi = useStrapi();
-  const {token, authState} = useAuth();
+  const {token, authState, updateProfile} = useAuth();
   const [connected, setConnected] = useState(false);
-
-  const addEvent = async event => {
-    if (connected) {
-      const {user} = authState;
-      const {events = []} = user;
-      // TODO: use authState.updateProfile
-      await strapi.services.users.update('me', {
-        ...user,
-        events: [...events.filter(e => e !== event.id), event.id],
-      });
-    }
-  };
 
   useEffect(() => {
     setConnected(!!token);
   }, [token]);
+
+  const addEvent = async event => {
+    if (connected) {
+      const {user} = authState;
+      const {events} = user;
+      updateProfile({
+        events: !!events
+          ? [...events.filter(e => e !== event.id), event.id]
+          : [event.id],
+      });
+    }
+  };
 
   return {connected, addEvent};
 };
