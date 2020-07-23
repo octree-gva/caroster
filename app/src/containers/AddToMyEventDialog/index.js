8c23fd0a69114a37beb1a0ad8119eb4eda9ac842
Karian Før <karian.for@gmail.com>
2020-07-23 16:15:36 +0200

    cleanup containers folder, fix redirect

--- app/src/containers/AddToMyEventDialog/index.js
+++ app/src/containers/AddToMyEventDialog/index.js
@@ -1,2 +1,61 @@
-import AddToMyEventDialog from './AddToMyEventDialog';
+import React, {useState} from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import Button from '@material-ui/core/Button';
+import {useTranslation} from 'react-i18next';
+import {Redirect} from 'react-router-dom';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const AddToMyEventDialog = ({event, open, onClose}) => {
+  const {t} = useTranslation();
+  const [redirectTo, setRedirectTo] = useState(null);
+
+  if (!event) return null;
+
+  if (redirectTo) {
+    return (
+      <Redirect push to={{pathname: redirectTo, state: {event: event.id}}} />
+    );
+  }
+
+  return (
+    <Dialog open={open} TransitionComponent={Transition} onClose={onClose}>
+      <DialogContent>
+        <DialogTitle>
+          {t('event.add_to_my_events.title', {eventName: event.name})}
+        </DialogTitle>
+        <DialogContentText
+          dangerouslySetInnerHTML={{
+            __html: t('event.add_to_my_events.text_html', {
+              eventName: event.name,
+            }),
+          }}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} id="AddToMyEventCancel">
+          {t('event.add_to_my_events.cancel')}
+        </Button>
+        <Button id="AddToMyEventLogin" onClick={() => setRedirectTo('/login')}>
+          {t('event.add_to_my_events.login')}
+        </Button>
+        <Button
+          id="AddToMyEventRegister"
+          onClick={() => setRedirectTo('/register')}
+          color="primary"
+        >
+          {t('event.add_to_my_events.register')}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
 export default AddToMyEventDialog;
--- app/src/containers/EventMenu/index.js
+++ app/src/containers/EventMenu/index.js
@@ -1,2 +1,49 @@
-import EventMenu from './EventMenu';
+import React from 'react';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import {useTranslation} from 'react-i18next';
+import {useStrapi} from 'strapi-react-context';
+
+const EventMenu = ({anchorEl, setAnchorEl, actions = []}) => {
+  const {t} = useTranslation();
+  const strapi = useStrapi();
+  const [settings] = strapi.stores?.settings || [{}];
+
+  const aboutMenuItem = {
+    label: t('menu.about'),
+    onClick: () => (window.location.href = settings['about_link']),
+    id: 'AboutTabs',
+  };
+
+  return (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={!!anchorEl}
+      onClose={() => setAnchorEl(null)}
+    >
+      {actions &&
+        [...actions, aboutMenuItem].filter(Boolean).map((action, idx) => (
+          <MenuItem
+            onClick={() => {
+              action.onClick();
+              setAnchorEl(null);
+            }}
+            key={idx}
+            id={action.id || `MenuItem${idx}`}
+          >
+            {action.label}
+          </MenuItem>
+        ))}
+    </Menu>
+  );
+};
 export default EventMenu;
