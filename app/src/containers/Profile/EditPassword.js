1e0a707e98d1f5ba20385431c243f3e9e97365b8
Hadrien Froger <hadrien@octree.ch>
2020-08-14 13:24:13 +0100

    :rotating_light: fix linter

--- app/src/containers/Profile/EditPassword.js
+++ app/src/containers/Profile/EditPassword.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import {useTranslation} from 'react-i18next';
 import TextField from '@material-ui/core/TextField';
-import IconButton from '@material-ui/core/IconButton';
-import Icon from '@material-ui/core/Icon';
 
 const EditPassword = ({
   oldPassword,
--- app/src/containers/Profile/index.js
+++ app/src/containers/Profile/index.js
@@ -1,9 +1,6 @@
 import React, {useState} from 'react';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
-import IconButton from '@material-ui/core/IconButton';
-import Icon from '@material-ui/core/Icon';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import {useTranslation} from 'react-i18next';
@@ -124,14 +121,19 @@ const Profile = ({profile, updateProfile, logout}) => {
         </CardContent>
         <CardActions className={classes.actions}>
           {!isEditing && (
-            <Button
-              type="button"
-              color="primary"
-              onClick={() => setIsEditing(true)}
-              variant="contained"
-            >
-              {t('profile.actions.edit')}
-            </Button>
+            <>
+              <Button type="button" onClick={() => logout()}>
+                {t('profile.actions.logout')}
+              </Button>
+              <Button
+                type="button"
+                color="primary"
+                onClick={() => setIsEditing(true)}
+                variant="contained"
+              >
+                {t('profile.actions.edit')}
+              </Button>
+            </>
           )}
           {isEditing && (
             <Button
