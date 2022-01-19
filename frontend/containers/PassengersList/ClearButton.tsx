b4e4a01eef100beb301bf892d0050953223e5a3a
Simon Mulquin <simon@octree.ch>
2022-01-19 14:24:53 +0100

    feat(PassengersList): ♿️ skip passengers action icon on tab selection

--- frontend/containers/PassengersList/ClearButton.tsx
+++ frontend/containers/PassengersList/ClearButton.tsx
@@ -5,15 +5,16 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 interface Props {
   onClick?: () => void;
   icon: string;
+  tabIndex?: number;
 }
 
 const ClearButton = (props: Props) => {
-  const {icon, onClick} = props;
+  const {icon, onClick, tabIndex} = props;
 
   if (onClick)
     return (
       <ListItemSecondaryAction>
-        <IconButton size="small" color="primary" onClick={onClick}>
+        <IconButton size="small" color="primary" onClick={onClick} tabIndex={tabIndex}>
           <Icon>{icon}</Icon>
         </IconButton>
       </ListItemSecondaryAction>
