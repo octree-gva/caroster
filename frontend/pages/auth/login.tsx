c6e862fa2d016fcdef877fadafe6bd11e8e872f7
Tim Izzo <tim@octree.ch>
2022-10-07 13:21:18 +0000

    feat: ✨ Show message on email confirmation

--- frontend/pages/auth/login.tsx
+++ frontend/pages/auth/login.tsx
@@ -1,5 +1,4 @@
 import CardMedia from '@material-ui/core/CardMedia';
-import Divider from '@material-ui/core/Divider';
 import Card from '@material-ui/core/Card';
 import {useTranslation} from 'react-i18next';
 import Layout from '../../layouts/Centered';
@@ -8,18 +7,29 @@ import SignInForm from '../../containers/SignInForm';
 import LanguagesIcon from '../../containers/Languages/Icon';
 import {getSession} from 'next-auth/react';
 import pageUtils from '../../lib/pageUtils';
+import Typography from '@material-ui/core/Typography';
 
 interface PageProps {
   error?: string;
+  emailConfirmation?: boolean;
 }
 
 const Login = (props: PageProps) => {
+  const {emailConfirmation} = props;
   const {t} = useTranslation();
 
   return (
     <Layout menuTitle={t('signin.title')} displayMenu={false}>
       <Card>
         <CardMedia component={Logo} />
+        {emailConfirmation && (
+          <Typography
+            style={{marginBottom: '3rem'}}
+            variant="body2"
+            align="center"
+            color="textSecondary"
+          >{t`signin.emailConfirmation`}</Typography>
+        )}
         <SignInForm error={props?.error} />
       </Card>
       <LanguagesIcon />
@@ -40,7 +50,8 @@ export const getServerSideProps = async (context: any) => {
   else
     return pageUtils.getServerSideProps(async ctx => {
       const error = ctx.query?.error || null;
-      return {props: {error}};
+      const emailConfirmation = ctx.query?.confirmed === 'true';
+      return {props: {error, emailConfirmation}};
     })(context);
 };
 
