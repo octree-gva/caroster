cacd9caef6f8dc5e7d257e5c2653092a59060f65
Tim Izzo <tim@5ika.ch>
2023-08-18 17:59:39 +0200

    chore: :arrow_up: Upgrade dependencies

--- backend/config/admin.ts
+++ backend/config/admin.ts
@@ -1,8 +1,13 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT"),
+  },
+  transfer: {
+    token: {
+      salt: env("TRANSFER_TOKEN_SALT"),
+    },
   },
 });
