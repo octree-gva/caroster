6bfdac741a7ee015a9da60c8346986ca651bb97b
Simon Mulquin <simon@octree.ch>
2024-05-08 06:58:31 +0000

    feat: ✨ No confirmation when removing passenger from a vehicle

--- frontend/containers/Travel/PassengerActions.tsx
+++ frontend/containers/Travel/PassengerActions.tsx
@@ -1,5 +1,7 @@
+import {useState} from 'react';
 import {Box, Icon, IconButton} from '@mui/material';
 import usePermissions from '../../hooks/usePermissions';
+import RemovePassengerModal from './RemovePassengerModal';
 import useActions from './useActions';
 import {PassengerEntity, TravelEntity} from '../../generated/graphql';
 
@@ -11,37 +13,45 @@ type Props = {
 
 const PassengerActions = (props: Props) => {
   const {passenger, travel, setFocusPassenger} = props;
+  const [isRemovingPassenger, setIsRemovingPassenger] = useState(false);
   const {
     userPermissions: {canDeletePassenger, canSeePassengerDetails},
   } = usePermissions();
-  const actions = useActions({travel});
+  const {removePassengerFromTravel} = useActions({travel});
 
   return (
-    <Box display="flex">
-      {canDeletePassenger(passenger) && {
-          id: passenger.id,
-          attributes: {
-            ...passenger.attributes,
-            travel: {data: travel},
-          },
-        } && (
+    <>
+      <Box display="flex">
+        {canDeletePassenger(passenger) && {
+            id: passenger.id,
+            attributes: {
+              ...passenger.attributes,
+              travel: {data: travel},
+            },
+          } && (
+            <IconButton
+              color="primary"
+              onClick={() => setIsRemovingPassenger(true)}
+              tabIndex={-1}
+            >
+              <Icon>delete_outline</Icon>
+            </IconButton>
+          )}
+        {canSeePassengerDetails(passenger) && (
           <IconButton
             color="primary"
-            onClick={() => actions.removePassengerFromTravel(passenger.id)}
-            tabIndex={-1}
+            onClick={() => setFocusPassenger(passenger)}
           >
-            <Icon>delete_outline</Icon>
+            <Icon>info_outlined</Icon>
           </IconButton>
         )}
-      {canSeePassengerDetails(passenger) && (
-        <IconButton
-          color="primary"
-          onClick={() => setFocusPassenger(passenger)}
-        >
-          <Icon>info_outlined</Icon>
-        </IconButton>
-      )}
-    </Box>
+      </Box>
+      <RemovePassengerModal
+        passenger={isRemovingPassenger && passenger}
+        close={() => setIsRemovingPassenger(false)}
+        removePassenger={removePassengerFromTravel}
+      />
+    </>
   );
 };
 
---
+++ frontend/containers/Travel/RemovePassengerModal.tsx
@@ -0,0 +1,70 @@
+import {useTranslation} from 'react-i18next';
+import {
+  Typography,
+  Dialog,
+  Button,
+  DialogContent,
+  DialogActions,
+  DialogTitle,
+} from '@mui/material';
+import useProfile from '../../hooks/useProfile';
+import useEventStore from '../../stores/useEventStore';
+import {PassengerEntity} from '../../generated/graphql';
+
+interface Props {
+  passenger: PassengerEntity;
+  close: () => void;
+  removePassenger: (id: string) => void;
+}
+
+const RemovePassengerModal = ({passenger, close, removePassenger}: Props) => {
+  const {t} = useTranslation();
+  const event = useEventStore(s => s.event);
+  const isCarosterPlus = event.enabled_modules?.includes('caroster-plus');
+  const {userId} = useProfile();
+  const IsPassengerIsUser = passenger.attributes?.user?.data?.id === userId;
+
+  const getDescriptionKey = () => {
+    if (IsPassengerIsUser && isCarosterPlus) {
+      return 'travel.removePassengerModal.plus.self.description';
+    } else if (isCarosterPlus) {
+      return 'travel.removePassengerModal.plus.description';
+    } else  if (IsPassengerIsUser) {
+      return 'travel.removePassengerModal.self.description';
+    } else {
+      return 'travel.removePassengerModal.description';
+    }
+  };
+
+  return (
+    <Dialog open={!!passenger} onClose={close}>
+      <DialogTitle>
+        {t(
+          IsPassengerIsUser
+            ? 'travel.removePassengerModal.self.title'
+            : 'travel.removePassengerModal.title'
+        )}
+      </DialogTitle>
+      <DialogContent>
+        <Typography>{t(getDescriptionKey())}</Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={close}>
+          {t('travel.removePassengerModal.cancel')}
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => removePassenger(passenger.id)}
+        >
+          {t(
+            IsPassengerIsUser
+              ? 'travel.removePassengerModal.self.remove'
+              : 'travel.removePassengerModal.remove'
+          )}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default RemovePassengerModal;
