d6b4f7eafc29c41d251109461090c1ccf6bab922
Tim Izzo <tim@octree.ch>
2024-11-05 10:58:15 +0100

    fix: :bug: Fix time display in notifications drawer
    #544

--- frontend/containers/DrawerNotification/formatDate.ts
+++ frontend/containers/DrawerNotification/formatDate.ts
@@ -6,9 +6,6 @@ export const formatDate = (dateString: string) => {
   const isToday = momentDate.isSame(moment(), 'day');
   const {t} = useTranslation();
 
-  if (isToday) {
-    return `${t('date.today')}, ${momentDate.format('hh:mm')}`;
-  } else {
-    return momentDate.format('DD/MM/YY, H:mm');
-  }
+  if (isToday) return `${t('date.today')}, ${momentDate.format('LT')}`;
+  else return momentDate.format('DD/MM/YY, LT');
 };
